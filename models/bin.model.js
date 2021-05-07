fe176116558f945802b2b1f3f71f632e2e80a3b7
Tejasvp25 <tejasvp25@gmail.com>
2021-05-07 18:49:11 +0530

    bin model : Include timestamp too

--- models/bin.model.js
+++ models/bin.model.js
@@ -2,14 +2,17 @@ const mongoose = require("mongoose");
 
 const Bin = mongoose.model(
   "bins",
-  new mongoose.Schema({
-    _id: String,
-    data: String,
-    owner_id: String,
-    owner_username: String,
-    private: Boolean,
-    shared_with: [{ type: String }],
-  })
+  new mongoose.Schema(
+    {
+      _id: String,
+      data: String,
+      owner_id: String,
+      owner_username: String,
+      private: Boolean,
+      shared_with: [{ type: String }],
+    },
+    { timestamps: true }
+  )
 );
 
 module.exports = Bin;
