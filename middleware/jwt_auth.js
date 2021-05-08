53582e81807701afe0e54efa2fe9fd3937a52625
Tejasvp25 <tejasvp25@gmail.com>
2021-05-08 21:40:47 +0530

    Cleanup

--- middleware/jwt_auth.js
+++ middleware/jwt_auth.js
@@ -2,33 +2,27 @@ const jwt = require("jsonwebtoken");
 const secret = process.env.SECRET || "secretkey";
 
 module.exports = function authToken(req, res, next) {
-  if (req.body === undefined) {
-    return res.status(400).end();
-  }
-  if (req.method === "POST" && !req.body.private) {
-    return next();
-  }
+  if (req.body === undefined) return res.status(400).end();
+
+  if (req.method === "POST" && !req.body.private) return next();
 
   const token =
-    req.method === "POST" || req.method === "DELETE" || req.method === "PUT"
-      ? req.body.Authorization !== undefined
-        ? req.body.Authorization.split(" ")[1]
+    req.method === "GET"
+      ? req.headers.authorization !== undefined
+        ? req.headers.authorization.split(" ")[1]
         : undefined
-      : req.headers.authorization !== undefined
-      ? req.headers.authorization.split(" ")[1]
+      : req.body.Authorization !== undefined
+      ? req.body.Authorization.split(" ")[1]
       : undefined;
 
-  if (!token && req.method === "GET" && req.route.path === "/api/bin/:binId") {
+  if (!token && req.method === "GET" && req.route.path === "/api/bin/:binId")
     return next();
-  }
 
-  if (!token) {
-    return res.status(400).send();
-  }
+  if (!token) return res.status(400).send();
+
   jwt.verify(token, secret, (err, decoded) => {
-    if (err) {
-      return res.status(401).send();
-    }
+    if (err) return res.status(401).send();
+
     req.body.owner_id = decoded.id;
     req.body.username = decoded.username;
     req.body.email = decoded.email;
