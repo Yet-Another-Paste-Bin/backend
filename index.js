e6aa26fad40a85ebf98d3bf13d67a802b1666705
Tejasvp25 <tejasvp25@gmail.com>
2021-05-11 19:01:04 +0530

    Use helmet.js to set some headers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const helmet = require("helmet");
 
 // Get Port Number from Env (if exists) else set to 8000
 const PORT = process.env.PORT || 8000;
@@ -10,9 +11,9 @@ const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/yapb";
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+app.use(helmet());
 const env = process.env.NODE_ENV || "production";
-if(env === "development"){
+if (env === "development") {
   app.use(require("cors")());
 }
 
